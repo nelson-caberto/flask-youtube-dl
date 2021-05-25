cf0f210114569cfc81ad6cc9bc670bd1225c3d21
nelson-caberto <nelson610@live.missouristate.edu>
2021-05-24 22:28:17 -0500

    created add route

--- flask-youtube-dl.py
+++ flask-youtube-dl.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def main():
     return "starter code"
 
+@app.route('/<site>/<filename>')
+def add(site, filename):
+    return f"{site} {filename}"
+
 if __name__ == "__main__":
     app.run(debug=True)
