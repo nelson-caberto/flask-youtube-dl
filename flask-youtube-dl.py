54dd4fae38e89e8cb4224e9c1552f7155078c0aa
nelson-caberto <nelson610@live.missouristate.edu>
2021-05-25 16:58:34 -0500

    refactored

--- flask-youtube-dl.py
+++ flask-youtube-dl.py
@@ -15,6 +15,9 @@ class logger(object):
 def hook(d):
     print(d)
 
+def decode(base):
+    return str(b64decode(base),'utf-8')
+
 @app.route('/')
 def main():
     return jsonify(queue)
@@ -32,12 +35,16 @@ def download():
 
 @app.route('/<site>', methods=['GET'])
 def add(site):
+    '''
+        requires all GET parameters to be base64 encoded
+        encoding is to eliminate the need to work around special characters
+    '''
     if site not in queue:
         queue[site] = []
     queue[site].append({
-        'filepath': request.args['filepath'],
-        'filename': request.args['filename'],
-        'url': str(b64decode(request.args['url']),'utf-8')
+        'filepath': decode(request.args['filepath']),
+        'filename': decode(request.args['filename']),
+        'url':      decode(request.args['url'])
     })
     download()
     return jsonify({'success':True}), 200, {'ContentType':'application/json'}
